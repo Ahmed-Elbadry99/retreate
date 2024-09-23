e49d61aaf8501986040cf4dccc8ef3040242fba6
elbadry <elbadry.egypt010@gmail.com>
2024-09-23 10:27:20 +0300

    add offers and rent details

--- js/custome2.js
+++ js/custome2.js
@@ -23,4 +23,37 @@
 
     // owl carousel for nav property card
 
+
+
+    // amount and precentage
+
+    discountType = document.getElementById("discount-type");
+    percentage = document.querySelector(".percentage");
+    amount = document.querySelector(".amount");
+    if(discountType){
+        discountType.addEventListener("change",()=>{
+            if(discountType.value =="percentage"){
+                percentage.style = "display:block"
+                amount.style = "display:none";
+            }
+            else{
+                amount.style = "display:block"
+                percentage.style = "display:none";
+            }
+        })
+    }
    
+
+
+
+    //  date input
+const dateInput = document.getElementById("dateInput");
+const calendarIcon = document.getElementById("calendarIcon");
+
+if(dateInput){
+    calendarIcon?.addEventListener("click", function () {
+        dateInput.showPicker();  
+        console.log(',c,bv')
+      });
+      
+}
